82ae8ddf83fb6541127bf30f357627cee84ed8a2
Mark Nesovanovic <mark.nesovanovic@gmail.com>
2025-09-15 21:09:58 +0200

    added images and basic image switching function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./style.css";
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function switchImage(imageDiv, index) {
+    Array.from(imageDiv.children).forEach(node => node.style.display = "none");
+    imageDiv.children[index].style.display = "block";
+}
+
+function changeImageOnTimer(imageDiv, timer, index) {
+    if(index == -1) {
+        // sleep(5000);
+        // changeImageOnTimer(imageDiv,timer,imageDiv.children.length-1);
+        return;
+    }
+    else {
+        switchImage(imageDiv, index);
+        sleep(timer);
+        changeImageOnTimer(imageDiv,timer,index-1);
+    }
+}
+
+switchImage(document.querySelector(".slider"),0);
+while(true) {
+    // changeImageOnTimer(document.querySelector(".slider"),5000,document.querySelector(".slider").children.length-1);
+}
